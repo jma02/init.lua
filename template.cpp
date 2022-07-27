2badc3984fe745d394b8b62581eefbe2f73d5fd0
johnma <ma.jonathan02@gmail.com>
2022-07-27 02:13:22 -0400

    added some stuff to main func

--- template.cpp
+++ template.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 void solve(){
-
+    
 }
 
 int main(){
     ios_base::sync_with_stdio(false);  
     cin.tie(NULL);
-    
+    int t; cin >> t;
+    while(t--) solve();
 }
